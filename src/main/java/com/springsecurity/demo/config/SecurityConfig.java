5d69dc24ecc34ff030e0559943d457fbcd5aed27
Jimin Kim <0305jimin@gmail.com>
2024-03-23 17:16:27 +0900

    Fix: using available symbols instead of deprecated ones
    even though the red line  still exists under 'csrf', it seems to be able to be ignored

--- src/main/java/com/springsecurity/demo/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/demo/config/SecurityConfig.java
@@ -2,20 +2,45 @@ package com.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// -> deprecated
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/user/**").authenticated() 
-                .antMatchers("/manager/**").access("hasAnyRole('ROLE_MANAGER','ROLE_ADMIN')")
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll();
+        http.csrf().disable(); // red line occurs but can be ignored
+        http.authorizeHttpRequests(authorize -> authorize
+
+                // when the client requests with /user/** url, authentication is needed
+                .requestMatchers("/user/**").authenticated()
+
+                // when the client requests with /manager/** url, the ones whose roles are ADMIN or MANAGER can only access
+                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
+
+                // when the client requests with /admin/** url, the ones whose roles are ADMIN can only access
+                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+
+                // for other requests, permit all
+                .anyRequest().permitAll());
 
         return http.build();
     }
+
+    // DEPRECATED
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.authorizeRequests()
+//                .antMatchers("/user/**").authenticated() //
+//                .antMatchers("/manager/**").access("hasAnyRole('ROLE_MANAGER','ROLE_ADMIN')")
+//                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+//                .anyRequest().permitAll();
+//
+//        return http.build();
+//    }
 }
