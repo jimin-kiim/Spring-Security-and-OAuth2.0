6ec514b9e84d55ea2f3aacdc3bc6d57cf90ccdc8
Jimin Kim <0305jimin@gmail.com>
2024-03-27 21:49:58 +0900

    Refactor: User to use `@NoArgsConstructor` annotation instead of
    adding default constructor

--- src/main/java/com/springsecurity/demo/model/User.java
+++ src/main/java/com/springsecurity/demo/model/User.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 
 @Entity // notating that it's JPA entity. would be mapped with table of db
 @Data // Getter, Setter, toString, equals, hashCode can be used
+@NoArgsConstructor
 public class User {
 
     @Id // primary key
@@ -35,9 +37,5 @@ public class User {
         this.provider = provider;
         this.providerId = providerId;
         this.createDate = createDate;
-    }
-
-    public User() {
-
     }
 }
