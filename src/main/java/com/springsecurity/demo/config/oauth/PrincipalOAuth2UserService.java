e031973e0ba61b25057ac10c6d77cf10952898f8
Jimin Kim <0305jimin@gmail.com>
2024-03-27 21:48:34 +0900

    Feat: implementing PrincipalOAuth2UserService to execute joining process
    by getting informations from OAuth2UserRequest object and saving it using repository

---
+++ src/main/java/com/springsecurity/demo/config/oauth/PrincipalOAuth2UserService.java
@@ -0,0 +1,60 @@
+package com.springsecurity.demo.config.oauth;
+
+import com.springsecurity.demo.config.auth.PrincipalDetails;
+import com.springsecurity.demo.model.User;
+import com.springsecurity.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    // executed after google oauth login is completed
+    // handling userRequest from google oauth
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        /*
+            System.out.println("userRequest: " + userRequest); // with registrationId inside, we can figure out how login processed with which host
+            System.out.println("getAccessToken: " + userRequest.getAccessToken());
+            System.out.println("getTokenValue: " + userRequest.getAccessToken().getTokenValue());
+            System.out.println("getClientRegistration: " + userRequest.getClientRegistration());
+            System.out.println("getAttributes: " + super.loadUser(userRequest).getAttributes());
+         */
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println("getAttributes: " + oAuth2User.getAttributes());
+
+        String provider = userRequest.getClientRegistration().getClientId();
+        String providerId = oAuth2User.getAttribute("sub");
+        String username = provider + "_" + providerId;
+        String password = bCryptPasswordEncoder.encode("password");
+        String email = oAuth2User.getAttribute("email");
+        String role = "ROLE_USER";
+
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity == null) {
+            userEntity = User.builder()
+                    .username(username)
+                    .password(password)
+                    .email(email)
+                    .role(role)
+                    .provider(provider)
+                    .providerId(providerId)
+                    .build();
+            userRepository.save(userEntity);
+        }
+
+        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
+    }
+}
--- src/main/java/com/springsecurity/demo/model/User.java
+++ src/main/java/com/springsecurity/demo/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -20,6 +21,23 @@ public class User {
     private String password;
     private String email;
     private String role; // ROLE_USER, ROLE_ADMIN
+    private String provider;
+    private String providerId;
     @CreationTimestamp
     private Timestamp createDate;
+
+    @Builder
+    public User(String username, String password, String email, String role, String provider, String providerId, Timestamp createDate) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.createDate = createDate;
+    }
+
+    public User() {
+
+    }
 }
